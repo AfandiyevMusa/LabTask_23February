2860a965e469b023a30753aa3cd8d0ac765e00b9
AfandiyevMusa <113282393+AfandiyevMusa@users.noreply.github.com>
2023-02-25 00:49:10 +0400

    2 labtasks are ready

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+//#region Task1
+// let eachcard = document.querySelectorAll("#cardsP .container .eachCard");
+// for (let i = 0; i < eachcard.length; i++) {
+//     eachcard[i].addEventListener("click", function () {
+//         // eachcard[i].classList.add("active")
+
+//         if (eachcard[i].classList.contains("active")) {
+//             eachcard[i].classList.remove("active");
+//         } else {
+//             eachcard[i].classList.add("active");
+//         }
+//     })
+// }
+//#endregion
+
+//#region Task2
+// let inputdiv = document.querySelector(".input-part")
+// let create = document.querySelector(".input-part button")
+// let inputAdd = document.querySelector(".input-part .upPart input")
+// let outputAdd = document.querySelector(".input-part .lastPart input")
+
+// let alertmessage = document.querySelector(".alert-part")
+
+// let words = document.querySelector(".alert-part .message span")
+
+// create.addEventListener("click", function(){
+//     if(inputAdd.value == "" && !alertmessage.classList.contains("display-part") && !inputdiv.classList.contains("container-add")){
+//         alertmessage.classList.add("display-part");
+//         inputdiv.classList.add("container-add")
+//     } else{
+//         outputAdd.value = inputAdd.value;
+//         inputAdd.value = ""
+//         let closeButton = document.querySelector(".alert-part .button-part i")
+//         closeButton.addEventListener("click", function(){
+//             alertmessage.classList.remove("display-part")
+//             inputdiv.classList.remove("container-add")
+//             outputAdd.value = ""
+//         })
+//     }
+// })
+//#endregion
+
+//#region Task3
+
+//#endregion
+
